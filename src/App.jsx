72928088c84832429c88c411161e03e87c50aa37
MProducciones <mentalistaproducciones@gmail.com>
2025-09-23 19:31:29 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+﻿import React from 'react'; // <--- Añade esta línea
+import ColorPlaneGame from './components/ColorPlaneGame';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ColorPlaneGame />
+    </div>
+  );
+}
+
+export default App;
